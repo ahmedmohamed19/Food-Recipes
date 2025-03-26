a0b475501be20ca79c79f4fcdec0f5661c6b0cc0
ahmedmohamed19 <ahmedmohamedahmeed6@gmail.com>
2025-03-26 07:48:39 +0200

    Social Icons Update

--- src/Components/SocialMedia/SocialMedia.jsx
+++ src/Components/SocialMedia/SocialMedia.jsx
@@ -1,15 +1,23 @@
 import React from "react";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 import styles from "./SocialMedia.module.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 export default function SocialMedia() {
     return (
         <div className={styles.socialIcons}>
-            <a href="#"><i className="fab fa-facebook-f"></i></a>
-            <a href="#"><i className="fab fa-twitter"></i></a>
-            <a href="#"><i className="fab fa-instagram"></i></a>
-            <a href="#"><i className="fab fa-youtube"></i></a>
+            <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" data-platform="facebook">
+                <i className="fab fa-facebook-f"></i>
+            </a>
+            <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" data-platform="twitter">
+                <i className="fab fa-twitter"></i>
+            </a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" data-platform="instagram">
+                <i className="fab fa-instagram"></i>
+            </a>
+            <a href="https://youtube.com" target="_blank" rel="noopener noreferrer" data-platform="youtube">
+                <i className="fab fa-youtube"></i>
+            </a>
         </div>
     );
 }
